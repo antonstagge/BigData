7ed2dcd054e7f74921d1b48addf305f47d29fea8
Anton Stagge <antonstagge95@gmail.com>
2019-02-17 11:28:23 +0100

    Added more games to crawl to speed up process

--- project/steam/steam/spiders/csgo.py
+++ project/steam/steam/spiders/csgo.py
@@ -5,13 +5,30 @@ from steam.items import CsgoItem
 class CsgoSpider(scrapy.Spider):
     name = 'csgo'
     allowed_domains = ['steamcommunity.com']
-    start_urls = ['https://steamcommunity.com/app/730/positivereviews/?browsefilter=toprated',
-        'https://steamcommunity.com/app/730/negativereviews/?browsefilter=toprated']
-
-    page = {
-        'pos': 'page1',
-        'neg': 'page1'
-    }
+    start_urls = ['https://steamcommunity.com/app/730/positivereviews/?browsefilter=toprated', # CS:GO
+        'https://steamcommunity.com/app/730/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/271590/positivereviews/?browsefilter=toprated', # GTAV
+        'https://steamcommunity.com/app/271590/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/570/positivereviews/?browsefilter=toprated', # Dota
+        'https://steamcommunity.com/app/570/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/578080/positivereviews/?browsefilter=toprated', # PubG
+        'https://steamcommunity.com/app/578080/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/252490/positivereviews/?browsefilter=toprated', # Rust 
+        'https://steamcommunity.com/app/252490/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/346110/positivereviews/?browsefilter=toprated', # ARK 
+        'https://steamcommunity.com/app/346110/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/550/positivereviews/?browsefilter=toprated', # left4dead2 
+        'https://steamcommunity.com/app/550/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/304930/positivereviews/?browsefilter=toprated', # Unturned 
+        'https://steamcommunity.com/app/304930/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/555570/positivereviews/?browsefilter=toprated', # Infestation 
+        'https://steamcommunity.com/app/555570/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/201510/positivereviews/?browsefilter=toprated', # Flat out 3: Chaos & Destuction 
+        'https://steamcommunity.com/app/201510/negativereviews/?browsefilter=toprated',
+        'https://steamcommunity.com/app/577800/positivereviews/?browsefilter=toprated', # NBA 2k18
+        'https://steamcommunity.com/app/577800/negativereviews/?browsefilter=toprated']
+    
+    scrape_count = 0
 
     def parse(self, response):
         """ For the first page: 
@@ -20,14 +37,17 @@ class CsgoSpider(scrapy.Spider):
         Extract the form data, and then submit the for to get the next page
         of review. 
         """
-        pos = (response.url.split("/")[5] == "positivereviews")
+        url_split = response.url.split("/")
+        app_id = url_split[4]
         page = response.xpath('//div[@id="page1"]')
         review_cards = page.css('.apphub_Card')
         for card in review_cards:
             yield self.create_item(card)
+        self.scrape_count += 10
+
         form = response.xpath('//form[@id="MoreContentForm1"]')
-        data,_ = self.get_form_data(form)
-        yield scrapy.FormRequest(url='https://steamcommunity.com/app/730/homecontent/', formdata=data, callback=self.parse_form)
+        data = self.get_form_data(form)
+        yield scrapy.FormRequest(url='https://steamcommunity.com/app/' + app_id + '/homecontent/', formdata=data, callback=self.parse_form)
 
     def parse_form(self, response):
         """ The first div contains the review cards,
@@ -37,25 +57,21 @@ class CsgoSpider(scrapy.Spider):
         div_id = first_div.attrib['id']
 
         form = response.css('form')
-        data, label = self.get_form_data(form)
-        
-        if div_id == self.page[label]:
-            print("%s has already been at %s" % (label, div_id))
-            yield
+        data = self.get_form_data(form)
 
         review_cards = first_div.css('.apphub_Card')
         for card in review_cards:
             yield self.create_item(card)
-    
-        self.page[label] = div_id
-        print("%s is now at %s" % (label, div_id))
+        self.scrape_count += 10
+        print("Scrape count is: %d" % self.scrape_count)
+        if self.scrape_count > 2000:
+            return
         
-        yield scrapy.FormRequest(url='https://steamcommunity.com/app/730/homecontent/', formdata=data, callback=self.parse_form)
+        yield scrapy.FormRequest(url=response.url, formdata=data, callback=self.parse_form)
 
     def get_form_data(self, form):
         """ Extract all the form data 
         required for getting the next page.
-        Returns the formdata and label of review.
         """
         data = dict()
         for html_input in form.css('input'):
@@ -63,12 +79,7 @@ class CsgoSpider(scrapy.Spider):
             value = html_input.attrib['value']
             data[name] = value
         
-        label = ""
-        if data['appHubSubSection'] == "16":
-            label = 'pos'
-        else:
-            label = 'neg'
-        return data, label
+        return data
     
     def create_item(self, card):
         """ Create the csv item which
